f5a7139d3c7acf04fa260e968606f217b594fd6b
KIMJINMINININN <vmelove@naver.com>
2021-01-28 22:44:25 +0900

    01.28
    2021.01.28
    Do it React Ex

---
+++ do-it-example/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1 className="title">두잇! 리액트 시작하기</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ChildComponent2 from './03/ChildComponent2';
 import ChildProperty from './03/ChildProperty';
 import DefaultPropsComponent from './03/DefaultPropsComponent';
+import StateExample from './03/StateExample';
+import ForceUpdateExample from './03/ForceUpdateExample';
+import LifecycleExample from './03/LifecycleExample';
 
-class App extends React.Component{
-    render(){
-        return (
-            <div>
-                <ChildProperty>
-                    <div><span>자식 노드!!</span></div>
-                </ChildProperty>
-            </div>
-        )
-    }
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <LifecycleExample />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
